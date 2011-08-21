48faf342b62e7b66e293834b5e4ac81c272b7957
David S <soulnafein@gmail.com>
2011-08-21 20:11:17 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingInsuranceReviews::Application.config.secret_token = '5faea75d638c0f29a32447f6bad4568ad643d0d38cfaaa9e737507ea63c0d8c87383935ac780698ada81f32747bb522ab68f9312aeb9e9af73f60194f7c9b651'
